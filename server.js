3b7483c712064814e45b04d0276b8596d6cbd6c5
Bill Sorrentino <williamsorrentino@gmail.com>
2017-10-26 00:45:45 -0400

    have the app working finally, need to fix the results when you submit the survey

--- server.js
+++ server.js
@@ -1,3 +1,35 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+// needs to be called before the routes in order to work
+app.use(express.static(__dirname + './public'));
+
+//app.get('/', function (req, res) {
+//    res.render('home', {
+//        pageTitle: 'Home Page',
+//        welcomeMessage: 'Welcome to the jungle',
+//
+//    });
+//});
+
+//Router
+require("./routes/apiRoutes.js") (app);
+require("./routes/htmlRoutes.js") (app);
+
+//Listening to the port that was set up
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
