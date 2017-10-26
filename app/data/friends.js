6824e4d7184d78653bb2117764d125202f9be9ec
Bill Sorrentino <williamsorrentino@gmail.com>
2017-10-26 11:19:13 -0400

    some updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,15 @@
 var friends = [
 	{
-		name: 'Donald Trump',
-		photo: 'http://theresurgent.com/wp-content/uploads/2016/02/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners-1.jpg',
+		name: 'Homer Simpson',
+		photo: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiWrsWZpo7XAhXFKyYKHelcDhAQjRwIBw&url=http%3A%2F%2Fmariosonictheedsandfriendsrideoffical.wikia.com%2Fwiki%2FFile%3AHomer-simpson.jpg&psig=AOvVaw0NrWQ5wnZ58iuk-gdRdJmz&ust=1509107642090093',
 		scores: [1, 5, 1, 1, 5, 5, 5, 1, 5, 5]
 	},{
-		name: 'Alex Morgan',
-		photo: 'http://www.famousbirthdays.com/headshots/alex-morgan-3.jpg',
+		name: 'David Lee Roth',
+		photo: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjgnNeppo7XAhXDayYKHTMBCVsQjRwIBw&url=https%3A%2F%2Fdemonszone.com%2F2016%2F01%2F08%2Fplay-david-lee-roth-asteroids-like-2009%2F&psig=AOvVaw1myqlja32US0xCgr5bnXlv&ust=1509107673390485',
 		scores: [4, 1, 4, 5, 1, 1, 1, 5, 1, 1]
 	},{
-		name: 'Phil Dunphy',
-		photo: 'https://cdn.pastemagazine.com/www/blogs/lists/2012/07/20/phil-dunphy.jpg',
+		name: 'Lady Gaga',
+		photo: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwj-y46_po7XAhUBYCYKHTXeBWsQjRwIBw&url=http%3A%2F%2Fschmutzberlin.com%2Fshow%2Flady-gaga%2F&psig=AOvVaw2B5trU_6cY2E2rvv2-OQae&ust=1509107713438977',
 		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
 	},{
 		name: 'Natalie Coughlin',
