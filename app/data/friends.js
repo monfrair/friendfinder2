c76cac0b9761a2d7b586ae3cfa792d67ed7fa798
Bill Sorrentino <williamsorrentino@gmail.com>
2017-10-27 10:17:03 -0400

    trying to get app to work

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,28 @@
 var friends = [
 	{
 		name: 'Homer Simpson',
-		photo: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiWrsWZpo7XAhXFKyYKHelcDhAQjRwIBw&url=http%3A%2F%2Fmariosonictheedsandfriendsrideoffical.wikia.com%2Fwiki%2FFile%3AHomer-simpson.jpg&psig=AOvVaw0NrWQ5wnZ58iuk-gdRdJmz&ust=1509107642090093',
+		photo: 'https://vignette.wikia.nocookie.net/mariosonictheedsandfriendsrideoffical/images/a/ab/Homer-simpson.jpg/revision/latest?cb=20140320010101',
 		scores: [1, 5, 1, 1, 5, 5, 5, 1, 5, 5]
 	},{
-		name: 'David Lee Roth',
-		photo: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjgnNeppo7XAhXDayYKHTMBCVsQjRwIBw&url=https%3A%2F%2Fdemonszone.com%2F2016%2F01%2F08%2Fplay-david-lee-roth-asteroids-like-2009%2F&psig=AOvVaw1myqlja32US0xCgr5bnXlv&ust=1509107673390485',
+		name: 'OJ Simpson The Juice',
+		photo: 'http://assets.nydailynews.com/polopoly_fs/1.3342309.1500600384!/img/httpImage/image.jpg_gen/derivatives/landscape_1200/people-simpson.jpg',
 		scores: [4, 1, 4, 5, 1, 1, 1, 5, 1, 1]
 	},{
-		name: 'Lady Gaga',
-		photo: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwj-y46_po7XAhUBYCYKHTXeBWsQjRwIBw&url=http%3A%2F%2Fschmutzberlin.com%2Fshow%2Flady-gaga%2F&psig=AOvVaw2B5trU_6cY2E2rvv2-OQae&ust=1509107713438977',
+		name: 'Elon Musk',
+		photo: 'https://www.biography.com/.image/t_share/MTE1ODA0OTcxOTUyMDE0ODYx/elon-musk-20837159-1-402.png',
 		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
 	},{
-		name: 'Natalie Coughlin',
-		photo: 'http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/natalie-olympics.jpg',
+		name: 'Miss Arkansas',
+		photo: 'https://hips.hearstapps.com/cos.h-cdn.co/assets/16/22/04-miss-usa-2016-swimsuit.jpg',
 		scores: [5, 1, 5, 2, 1, 1, 1, 5, 1, 1]
 	}];
 
+
+
+
+
+
 module.exports = friends;
+
+
+
